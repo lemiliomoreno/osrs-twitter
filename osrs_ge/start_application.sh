4347c7f72f2c128935282abc6a71d4957b80fb0a
lemiliomoreno <lemiliomoreno@gmail.com>
2019-08-12 00:05:27 -0500

    Update start_application.sh

--- osrs_ge/start_application.sh
+++ osrs_ge/start_application.sh
@@ -7,6 +7,6 @@ export TWITTER_API_CONSUMER_SECRET="your CONSUMER_SECRET"
 export TWITTER_API_ACCESS_KEY="your ACCESS_KEY"
 export TWITTER_API_ACCESS_SECRET="your ACCESS_SECRET"
 
-export TWITTER_API_ACCOUNT_ID="201125030"
+export TWITTER_API_ACCOUNT_ID="your ACCOUNT_ID"
 
 python /home/osrs_manager/osrs_ge/get_dm.py >> osrs_manager__logs.txt
